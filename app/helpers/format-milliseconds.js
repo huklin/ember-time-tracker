07fa6298228715bc58c16e590b450669cf4da231
Joerick Lau <huklin@Mac-Daddy.local>
2017-05-13 17:05:02 -0400

    Add timer helper to convert milliseconds in counter to a stopwatch format

---
+++ app/helpers/format-milliseconds.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export function formatMilliseconds([ms, ...rest]) {
+  const s = Math.floor (ms/1000);
+  const hour = Math.floor (s/3600);
+  const min = Math.floor((s % 3600) / 60);
+  const sec = s % 60;
+
+  return padNumber(hour) + ':' + padNumber(min) + ':' + padNumber(sec);
+}
+
+function padNumber(num) {
+  if (num < 10) {
+    return `0${num}`;
+  }
+  return String(num);
+}
+
+export default Ember.Helper.helper(formatMilliseconds);
---
+++ tests/unit/helpers/format-milliseconds-test.js
@@ -0,0 +1,12 @@
+
+import { formatMilliseconds } from 'ember-timetracker/helpers/format-milliseconds';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format milliseconds');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = formatMilliseconds([42]);
+  assert.ok(result);
+});
+
